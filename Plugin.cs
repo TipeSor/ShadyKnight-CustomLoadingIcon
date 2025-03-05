04e4cd5a02fdef00cebf763ef499b4fc0d80ea9d
TipeSor <antanas8899@gmail.com>
2025-03-05 19:02:20 +0200

    stuff

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CustomLoadingIcon;
+
+[BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {PluginInfo.GUID} is loaded!");
+
+        Harmony harmony = new Harmony(PluginInfo.GUID);
+
+        harmony.PatchAll();
+    }
+
+    private void Start()
+    {
+        CommandHandler.InitializeCommands();
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,8 @@
+namespace CustomLoadingIcon;
+
+static class PluginInfo
+{
+    public const string NAME = "CLI";
+    public const string GUID = "tipe.shadyknight.customloadingicon";
+    public const string VERSION = "1.0.0";
+}
